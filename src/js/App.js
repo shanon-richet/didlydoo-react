e513d5a256054839db18b47b81c1ae216ebc6179
Richet <shanon.richet@gmail.com>
2021-11-25 16:26:56 +0100

    add form to submit event

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,12 @@
 import '../App.css';
 import UserData from './UserData';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
           <UserData />
       </header>
     </div>
